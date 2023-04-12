7a71e5ef08b69d25ea91d30d0ce1d21471059fa8
Spagecko <bennettlawrenz@gmail.com>
2023-04-11 20:35:09 -0700

    test commit

--- PDFSamperWpf/MainWindow.xaml.cs
+++ PDFSamperWpf/MainWindow.xaml.cs
@@ -12,13 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-namespace PDFSamperWpf
+using MahApps.Metro.Controls;
+namespace PDFStamperWpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow :  MetroWindow
     {
         public MainWindow()
         {
